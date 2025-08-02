1a362be47170f8e6d5627305b1cc2147656aecd4
Kevin Lam <kevin.lam72003@gmail.com>
2025-08-01 19:35:58 -0500

    Add FX cleanup

---
+++ gmtk25/Assets/Scripts/FX Support/KillAfterTime.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class KillAfterTime : MonoBehaviour
+{
+    [SerializeField] private float _killTime;
+
+    private float _timePassed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_killTime < _timePassed)
+        {
+            Destroy(gameObject);
+        }
+
+        _timePassed += Time.deltaTime;
+    }
+}
