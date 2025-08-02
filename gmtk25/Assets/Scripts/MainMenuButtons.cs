2a9640bee3bf3d914b9b540b97b06bce9f416516
rbguan <rbn.guan@gmail.com>
2025-08-02 15:08:42 -0500

    Initial Menu buttons

--- gmtk25/Assets/Scripts/MainMenuButtons.cs
+++ gmtk25/Assets/Scripts/MainMenuButtons.cs
@@ -10,10 +10,10 @@ public class MainMenuButtons : MonoBehaviour
     public AudioSource source;
     public AudioClip hover, click;
     [SerializeField] private TMPro.TMP_Text _text;
-    private DOTweenTMPAnimator _animator;
+    // private DOTweenTMPAnimator _animator;
     void Awake()
     {
-        _animator = new DOTweenTMPAnimator(_text);
+        // _animator = new DOTweenTMPAnimator(_text);
     }
     private void Start()
     {
@@ -47,16 +47,16 @@ public class MainMenuButtons : MonoBehaviour
     public void LetterLoadIn()
     {
         
-        Sequence sequence = DOTween.Sequence();
-        sequence.AppendInterval(1.5f);
-        for (int i = 0; i < _animator.textInfo.characterCount; ++i)
-        {
-            if (!_animator.textInfo.characterInfo[i].isVisible) continue;
-            Vector3 currCharOffset = _animator.GetCharOffset(i);
-            // sequence.AppendInterval(0.05f);
-            sequence.Insert((float)i * .25f,_animator.DOOffsetChar(i, currCharOffset + new Vector3(2000, 0, 0), 1).From());
-        }
-        sequence.Play();
+        // Sequence sequence = DOTween.Sequence();
+        // sequence.AppendInterval(1.5f);
+        // for (int i = 0; i < _animator.textInfo.characterCount; ++i)
+        // {
+        //     if (!_animator.textInfo.characterInfo[i].isVisible) continue;
+        //     Vector3 currCharOffset = _animator.GetCharOffset(i);
+        //     // sequence.AppendInterval(0.05f);
+        //     sequence.Insert((float)i * .25f,_animator.DOOffsetChar(i, currCharOffset + new Vector3(2000, 0, 0), 1).From());
+        // }
+        // sequence.Play();
     }
 
 
