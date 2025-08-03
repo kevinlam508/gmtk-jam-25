26c806aa4426726cd21a9aea454f1aeae26cbc0c
hexxag0nn <58057979+hexxag0nn@users.noreply.github.com>
2025-08-03 04:27:37 -0500

    player health bar changes + hookup

--- gmtk25/Assets/Scripts/UI/PlayerHealth.cs
+++ gmtk25/Assets/Scripts/UI/PlayerHealth.cs
@@ -1,12 +1,34 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
+    [SerializeField] private Color _mediumHurt;
+    [SerializeField] private Color _hurt;
+    [SerializeField] private Image _fill;
+    private Color _defaultColor;
+
+    void Start()
+    {
+        _defaultColor = _fill.color;
+    }
 
     public void OnHealthChange(int current, int total)
     {
         _slider.value = (float)current / total;
+
+        if (_slider.value <= 0.7f)
+        {
+            _fill.color = _mediumHurt;
+            if (_slider.value <= 0.3f) _fill.color = _hurt;
+        }
+        else _fill.color = _defaultColor;
+    }
+
+    public void OnDamageTaken()
+    {
+        _slider.transform.DOShakePosition(.3f, 10);
     }
 }
