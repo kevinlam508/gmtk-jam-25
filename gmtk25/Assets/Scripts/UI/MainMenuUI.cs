7f3ec2912191b3a0c0308965fef9bca61b5cfe61
rbguan <rbn.guan@gmail.com>
2025-08-03 00:36:48 -0500

    title screen changes

--- gmtk25/Assets/Scripts/UI/MainMenuUI.cs
+++ gmtk25/Assets/Scripts/UI/MainMenuUI.cs
@@ -12,8 +12,11 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private AnimationCurve _settingsUiElementsFadeIn;
     [SerializeField] private RectTransform _settingsUiElements;
     [SerializeField] private Image _settingsUiBG;
-    [SerializeField][Range(0,1)] private float _settingsUiY;
-    [SerializeField][Range(0,2)] private float _settingsUiTweenTime;
+    [SerializeField] private GameObject _creditsWindow;
+    [SerializeField] private RectTransform _creditsUiElements;
+    [SerializeField] private Image _creditsUiBG;
+    [SerializeField][Range(0, 1)] private float _settingsUiY;
+    [SerializeField][Range(0, 2)] private float _settingsUiTweenTime;
     void Awake()
     {
         _settingsWindow.SetActive(false);
@@ -50,6 +53,33 @@ public class MainMenuUI : MonoBehaviour
         Sequence TweenInSequence = DOTween.Sequence();
         TweenInSequence.AppendInterval(.5f);
         TweenInSequence.Append(_settingsUiElements.DOMoveY(Screen.height * _settingsUiY, _settingsUiTweenTime).SetEase(_settingsUiElementsCurveIn));
+        TweenInSequence.Play();
+    }
+    public void OnCreditsOpened()
+    {
+        _creditsWindow.SetActive(true);
+        _creditsUiBG.DOFade(.4f, 1f).SetEase(_settingsUiElementsFadeIn);
+        TweenIn_Credits();
+    }
+
+    public void OnCreditsClosed()
+    {
+        StartCoroutine(FadeOutCreditsCoroutine());
+    }
+    private IEnumerator FadeOutCreditsCoroutine()
+    {
+        Tween myTween = _creditsUiBG.DOFade(0f, 1f).SetEase(_settingsUiElementsFadeIn);
+        _creditsUiElements.DOMoveY(Screen.height * 1.5f, _settingsUiTweenTime).SetEase(_settingsUiElementsCurveIn);
+        yield return myTween.WaitForCompletion();
+        _creditsWindow.SetActive(false);
+        // This log will happen after the tween has completed
+        Debug.Log("Tween completed!");
+    }
+    public void TweenIn_Credits()
+    {
+        Sequence TweenInSequence = DOTween.Sequence();
+        TweenInSequence.AppendInterval(.5f);
+        TweenInSequence.Append(_creditsUiElements.DOMoveY(Screen.height * _settingsUiY, _settingsUiTweenTime).SetEase(_settingsUiElementsCurveIn));
         TweenInSequence.Play();
     }
 }
