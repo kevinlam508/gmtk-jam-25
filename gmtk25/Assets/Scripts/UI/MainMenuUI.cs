60f5e0fa89fd6aee3e957657c8a5ce8c23d3af86
rbguan <rbn.guan@gmail.com>
2025-08-02 22:14:39 -0500

    started settings window

---
+++ gmtk25/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenuUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject _settingsWindow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
