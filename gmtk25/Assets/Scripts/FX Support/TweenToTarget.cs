841065a11a327554eb8e3f785727927397482f9b
Kevin Lam <kevin.lam72003@gmail.com>
2025-08-03 02:31:04 -0500

    Add chain kill timer callback

--- gmtk25/Assets/Scripts/FX Support/TweenToTarget.cs
+++ gmtk25/Assets/Scripts/FX Support/TweenToTarget.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TweenToTarget : MonoBehaviour
 {
     [SerializeField] private float _killTime = 1;
+    [SerializeField] private UnityEvent _killTimerBegan;
 
     private Vector3 _target;
     private float _duration;
 
     private float _timePassed;
     private Vector3 _start;
+
+    private bool _calledKillBegin;
 
     public void Init(Vector3 target, float duration)
     {
@@ -27,6 +31,12 @@ public class TweenToTarget : MonoBehaviour
         transform.position = Vector3.Lerp(_start, _target, _timePassed / _duration);
 
         _timePassed += Time.deltaTime;
+
+        if (!_calledKillBegin && _timePassed > _duration)
+        {
+            _killTimerBegan.Invoke();
+            _calledKillBegin = true;
+        }
 
         if (_timePassed > _duration + _killTime)
         {
