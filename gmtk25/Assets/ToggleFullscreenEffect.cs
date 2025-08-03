2c7e0ea7be2b7421d864fc50915b132447b03943
Justin <justin2jam@gmail.com>
2025-08-02 19:44:21 -0500

    Added screenspace hit FX

---
+++ gmtk25/Assets/ToggleFullscreenEffect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ToggleFullscreenEffect : MonoBehaviour
+{
+    public ScriptableRendererFeature screenFX;
+    public Material screenFXMaterial;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        screenFX.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            screenFX.SetActive(!screenFX.isActive);
+            Hit();
+        }
+    }
+
+    public void Hit() {
+        screenFXMaterial.SetFloat("_CurrentTimestamp", Time.time);
+    }
+}
