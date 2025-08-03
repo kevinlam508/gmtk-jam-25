778824bfd14f874fbf1e83c45a71c78633f6758a
Justin <justin2jam@gmail.com>
2025-08-02 23:29:15 -0500

    Added on hit screen fx

--- gmtk25/Assets/ToggleFullscreenEffect.cs
+++ gmtk25/Assets/ToggleFullscreenEffect.cs
@@ -8,18 +8,17 @@ public class ToggleFullscreenEffect : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        screenFX.SetActive(false);
+        // screenFX.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            screenFX.SetActive(!screenFX.isActive);
-            Hit();
-        }
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.T))
+    //     {
+    //         Hit();
+    //     }
+    // }
 
     public void Hit() {
         screenFXMaterial.SetFloat("_CurrentTimestamp", Time.time);
