4ae90fc121326b40a4a4be02e99204ba871ef89d
Kevin Lam <kevin.lam72003@gmail.com>
2025-08-04 16:42:21 -0500

    Undo some nerfs

--- gmtk25/Assets/Scripts/Charms/CharmData/CharmData.cs
+++ gmtk25/Assets/Scripts/Charms/CharmData/CharmData.cs
@@ -294,7 +294,7 @@ public class CharmData : ScriptableObject
         result._explosionCount += WeightedAdd(sub1?._explosionCount, sub2?._explosionCount, 1);
         result._explosionRadius += WeightedAdd(sub1?._explosionRadius, sub2?._explosionRadius, .5f);
         result._chainCount += WeightedAdd(sub1?._chainCount, sub2?._chainCount, 1);
-        result._chainRadius += WeightedAdd(sub1?._chainRadius, sub2?._chainRadius, .5f);
+        result._chainRadius += WeightedAdd(sub1?._chainRadius, sub2?._chainRadius, 1);
 
         result._damage += WeightedAdd(sub1?._damage, sub2?._damage, .75f);
         result._onHitStatus = CombineArrays(result._onHitStatus, sub1?._onHitStatus, sub2?._onHitStatus);
